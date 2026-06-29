3a785f4c52241d606a37916399a4969b0304940c
adam-spencer <adam.spencer2401@gmail.com>
2026-06-29 12:01:32 +0100

    Fixed autopairs + autolist, added eyeliner + leap

--- init.lua
+++ init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- ============================================================
 -- SECTION 1: OPTIONS
 -- Core Neovim settings, leaders, options, basic keymaps, basic autocmds
@@ -1001,11 +915,20 @@ do
   vim.keymap.set('i', '<tab>', '<cmd>AutolistTab<cr>')
   vim.keymap.set('i', '<s-tab>', '<cmd>AutolistShiftTab<cr>')
   -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
-  vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>')
+  -- vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>')
   vim.keymap.set('n', 'o', 'o<cmd>AutolistNewBullet<cr>')
   vim.keymap.set('n', 'O', 'O<cmd>AutolistNewBulletBefore<cr>')
   vim.keymap.set('n', '<CR>', '<cmd>AutolistToggleCheckbox<cr><CR>')
   vim.keymap.set('n', '<leader>ar', '<cmd>AutolistRecalculate<cr>')
+
+  -- Change autolist enter key to only work in md / text files - avoids issues with autopairs
+  vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'markdown', 'text' },
+    callback = function()
+      -- The { buffer = true } option gives this mapping priority over nvim-autopairs
+      vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>', { buffer = true })
+    end,
+  })
 
   -- cycle list types with dot-repeat
   vim.keymap.set('n', '<leader>cn', require('autolist').cycle_next_dr, { expr = true })
@@ -1066,11 +989,34 @@ end
 -- ============================================================
 do
   vim.pack.add { gh 'windwp/nvim-autopairs' }
-  require('nvim-autopairs').setup {
+  local npairs = require 'nvim-autopairs'
+  local Rule = require 'nvim-autopairs.rule'
+  npairs.setup {
     -- Enable treesitter integration so it checks context
     -- (e.g., it won't auto-close brackets if you're typing inside a string or comment)
     check_ts = true,
   }
+
+  npairs.add_rules {
+    Rule("b'", "'", 'rust'),
+  }
+end
+
+-- ============================================================
+-- SECTION XII: NAVIGATION
+-- Eyeliner (f/t unique letter highlighting)
+-- + Leap (move around page)
+-- ============================================================
+do
+  vim.pack.add { gh 'jinh0/eyeliner.nvim' }
+  require('eyeliner').setup {
+    highlight_on_key = true,
+    dim = true,
+  }
+
+  vim.pack.add { 'https://codeberg.org/andyg/leap.nvim' }
+  require('leap').setup {}
+  vim.keymap.set({ 'n', 'x', 'o' }, '<leader>n', '<Plug>(leap)')
 end
 
 -- ============================================================
